990b3642475d804be0e87dd223ddd8f3856e2c0c
Lucieguerin431 guerin <lucie.guerin@edu.univ-eiffel.fr>
2023-09-13 14:51:14 +0000

    first

---
+++ CMakeFiles/greet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greet.dir/main.cpp.o"
+  "CMakeFiles/greet.dir/main.cpp.o.d"
+  "bin/greet"
+  "bin/greet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
